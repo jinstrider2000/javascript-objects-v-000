33d5e1294dc1b64de9e53806e2e272b2c2be5628
Efrain Perez Jr <efrainperezjr@live.com>
2018-02-22 16:53:23 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
-playlist = new Object({u2: ["I still haven't found what I'm looking for", "with or without you"]})
-
+function objects() {
+  playlist = new Object({u2: ["I still haven't found what I'm looking for", "with or without you"]})
+}
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
   return playlist
